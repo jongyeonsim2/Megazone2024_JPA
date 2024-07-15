3b47035d84381b58d2fc23eeb5cbb828b8ea25fb
unknown <jongyeonsim2@gmail.com>
2024-07-15 12:08:46 +0900

    4st commit

--- Chapter02/src/main/java/com/rubypaper/biz/domain/Employee3.java
+++ Chapter02/src/main/java/com/rubypaper/biz/domain/Employee3.java
@@ -18,7 +18,10 @@ import lombok.Data;
  *    NAME, MAILID 의 두개의 칼럼으로 복합 unique 제약조건이 생성되었는지 확인
  * 2. uniqueConstraints 동작 테스트
  * 3. 테스트 데이터 생성
- *    hong, 홍길동
+ *    hong, 홍길동 : unique 제약 조건 위배
+ *    hong2, 홍길동 : insert 성공
+ *    hong, 홍길동3 : insert 성공
+ *    hong4, 홍길동4 : insert 성공
  *    
  * 그래서, 정말 중요한 것은 생성되는 데이터가 처음부터 깨끗하게 만들어지지
  * 않는다면, 향후에 개발중에서 어뚱한 데이터가 나오게 됨.
--- Chapter02/src/test/java/com/rubypaper/biz/client/Employee5ServiceClient.java
+++ Chapter02/src/test/java/com/rubypaper/biz/client/Employee5ServiceClient.java
@@ -7,9 +7,9 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import com.rubypaper.biz.domain.Employee3;
+import com.rubypaper.biz.domain.Employee5;
 
-public class Employee3ServiceClient {
+public class Employee5ServiceClient {
 
 	public static void main(String[] args) {
 		// <persistence-unit name="Chapter02"> 의 설정 정보를 참조
@@ -23,16 +23,18 @@ public class Employee3ServiceClient {
 		
 		try {
 			// 영속성 관리 엔터티 생성
-			Employee3 employee = new Employee3();
+			Employee5 employee = new Employee5();
 			
-			//employee.setId(1L);
-			employee.setId(4L);
-			employee.setName("홍길동4");
-			employee.setMailId("hong4");
+			employee.setId(1L);
+			employee.setName("홍길동");
+			employee.setMailId("hong");
 			employee.setStartDate(new Date());
 			employee.setTitle("대리");
 			employee.setDeptName("개발부");
 			employee.setSalary(2500.00);
+			
+			// commisionPct의 허용 값 : 10, 12.5, 15, 17.5, 20 
+			// Check constraint violation: "CONSTRAINT_4351
 			employee.setCommisionPct(12.50);
 			
 			// 트랜잭션 시작
